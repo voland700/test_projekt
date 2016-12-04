c0d8529fe5e7b77bef7d25db112690b9c4ddf75b
voland700 <voland700@yandex.ru>
2016-12-04 23:55:47 +0300

    Верстка аккардиона с вопросами - логика JS

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,20 @@
-$(function() {
+$(document).ready(function() {
+	var $ansver_blok = $('.ansver_blok');
+	
+	$ansver_blok.hide().prev().click(function(){
+		$ansver_blok.not(this).slideUp();
+		$(this).next().not(':visible').slideDown();
+	});//окончание Click
 
-	// Custom JS
+	$ansver_blok.on('click', '.ansver-item', function(){
+	var quest = $(this).data('ansver');		
+	if(quest===1){
+		$(this).css({'background': 'rgba(68, 252, 94, 0.4)'});
+		$(this).parent().parent().has('.question-item').removeClass('.question-item--false').addClass('question-item--true' );			
+	} else {
+		$(this).css({'background': 'rgba(251, 64, 37, 0.4)'});		
+	}
+	});
 
 });
+
